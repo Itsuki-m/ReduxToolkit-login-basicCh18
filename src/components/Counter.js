84608bcce563f45c0894405c44229e7856d7b61e
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-23 17:53:26 +0900

    split slices

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 
-import { counterActions } from '../store/index';
+import { counterActions } from '../store/counter-slice';
 import classes from './Counter.module.css';
 
 const Counter = () => {
@@ -43,45 +43,3 @@ const Counter = () => {
 };
 
 export default Counter;
-
-// class-based components and connect Fn
-// import { Component } from 'react';
-// import { connect } from 'react-redux';
-// class Counter extends Component {
-//   handleIncrement() {
-//     this.props.increment();
-//   }
-
-//   handleDecrement() {
-//     this.props.decrement();
-//   }
-
-//   toggleCounterHandler() {}
-
-//   render() {
-//     return (
-//       <main className={classes.counter}>
-//         <h1>Redux Counter</h1>
-//         <div className={classes.value}>{this.props.counter}</div>
-//         <div>
-//           <button onClick={this.handleIncrement.bind(this)}>Increment</button>
-//           <button onClick={this.handleDecrement.bind(this)}>Decrement</button>
-//         </div>
-//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-//       </main>
-//     );
-//   }
-// }
-
-// const mapStateToProps = (state) => {
-//   return { counter: state.counter };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     increment: () => dispatch({ type: 'INCREMENT' }),
-//     decrement: () => dispatch({ type: 'DECREMENT' }),
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/index.js
+++ src/store/index.js
@@ -1,110 +1,14 @@
 // redux
 
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counter-slice';
+import authReducer from './auth-slice';
 
 const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
-    auth: authSlice.reducer,
+    counter: counterReducer,
+    auth: authReducer,
   },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
-
 export default store;
-
-///////////////////////////////
-///////////////////////////////
-// import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-// const initialState = { counter: 0, showCounter: true };
-
-// const counterSlice = createSlice({
-//   name: 'counter',
-//   initialState,
-//   reducers: {
-//     increment(state) {
-//       state.counter++;
-//     },
-//     decrement(state) {
-//       state.counter--;
-//     },
-//     increase(state, action) {
-//       state.counter = state.counter + action.payload;
-//     },
-//     toggleCounter(state) {
-//       state.showCounter = !state.showCounter;
-//     },
-//   },
-// });
-
-// const store = configureStore({
-//   reducer: counterSlice.reducer,
-// });
-
-// export const counterActions = counterSlice.actions;
-
-// export default store;
-
-////////////////////////////////
-///////////////////////////////
-///////////////////////////////
-// import { createStore } from 'redux';
-
-// const counterReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case 'INCREMENT':
-//       return { counter: state.counter + 1, showCounter: state.showCounter };
-//     case 'INCREASE':
-//       return {
-//         counter: state.counter + action.amount,
-//         showCounter: state.showCounter,
-//       };
-//     case 'DECREMENT':
-//       return { counter: state.counter - 1, showCounter: state.showCounter };
-//     case 'TOGGLE':
-//       return { counter: state.counter, showCounter: !state.showCounter };
-//     default:
-//       return state;
-//   }
-// };
-// const store = createStore(counterReducer);
