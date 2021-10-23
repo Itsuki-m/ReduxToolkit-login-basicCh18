92e8b70027072884325de710c8c4eb2d439808db
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-23 18:06:57 +0900

    added class-based in COunter.js (commented-out)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -43,3 +43,45 @@ const Counter = () => {
 };
 
 export default Counter;
+
+// class-based components and connect Fn
+// import { Component } from 'react';
+// import { connect } from 'react-redux';
+// class Counter extends Component {
+//   handleIncrement() {
+//     this.props.increment();
+//   }
+
+//   handleDecrement() {
+//     this.props.decrement();
+//   }
+
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.handleIncrement.bind(this)}>Increment</button>
+//           <button onClick={this.handleDecrement.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return { counter: state.counter };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: 'INCREMENT' }),
+//     decrement: () => dispatch({ type: 'DECREMENT' }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
